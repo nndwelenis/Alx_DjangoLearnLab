243bdbcfff2b0b3899984f3edfda879ff7d45fc9
nndwelenis <nndwelenis@gmail.com>
2026-01-25 14:20:22 +0200

    Render role-based templates in RBAC views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -60,14 +60,14 @@ def is_member(user):
 
 @user_passes_test(is_admin)
 def admin_view(request):
-    return HttpResponse("Admin content")
+    return render(request, "relationship_app/admin_view.html")
 
 
 @user_passes_test(is_librarian)
 def librarian_view(request):
-    return HttpResponse("Librarian content")
+    return render(request, "relationship_app/librarian_view.html")
 
 
 @user_passes_test(is_member)
 def member_view(request):
-    return HttpResponse("Member content")
+    return render(request, "relationship_app/member_view.html")
