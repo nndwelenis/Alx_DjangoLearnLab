71e3ea215a6ed75e336ae60df090d9c4f5f87d85
nndwelenis <nndwelenis@gmail.com>
2026-01-25 14:28:11 +0200

    Align book permission URL paths with checker expectations

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -24,9 +24,9 @@ urlpatterns = [
     path("admin-role/", admin_view, name="admin_view"),
     path("librarian-role/", librarian_view, name="librarian_view"),
     path("member-role/", member_view, name="member_view"),
-    path("books/add/", add_book, name="add_book"),
-    path("books/edit/", edit_book, name="edit_book"),
-    path("books/delete/", delete_book, name="delete_book"),
+    path("add_book/", add_book, name="add_book"),
+    path("edit_book/", edit_book, name="edit_book"),
+    path("delete_book/", delete_book, name="delete_book"),
 
 
 ]
