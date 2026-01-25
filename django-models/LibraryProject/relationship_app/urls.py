4df9ecb107c50918568c68d2550b7ee93689f42f
nndwelenis <nndwelenis@gmail.com>
2026-01-25 14:11:58 +0200

    Align authentication URLs with Django built-in auth views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,12 +1,21 @@
 from django.urls import path
-from .views import list_books
-from .views import LibraryDetailView
-from .views import UserLoginView, UserLogoutView, register
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views
+from .views import list_books, LibraryDetailView
 
 urlpatterns = [
     path("books/", list_books, name="list_books"),
     path("libraries/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
-    path("login/", UserLoginView.as_view(), name="login"),
-    path("logout/", UserLogoutView.as_view(), name="logout"),
-    path("register/", register, name="register"),
+
+    path(
+        "login/",
+        LoginView.as_view(template_name="relationship_app/login.html"),
+        name="login",
+    ),
+    path(
+        "logout/",
+        LogoutView.as_view(template_name="relationship_app/logout.html"),
+        name="logout",
+    ),
+    path("register/", views.register, name="register"),
 ]
