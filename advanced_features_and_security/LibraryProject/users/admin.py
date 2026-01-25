4d70aeb5839d709de47ecb2bd50129ac606c25f9
nndwelenis <nndwelenis@gmail.com>
2026-01-25 11:56:46 +0200

    Implement custom user model with additional fields

---
+++ advanced_features_and_security/LibraryProject/users/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ("username", "email", "date_of_birth", "is_staff")
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional Information", {"fields": ("date_of_birth", "profile_photo")}),
+    )
