870dd85f02a8f6de99056199395d6f9835cdfb96
nndwelenis <nndwelenis@gmail.com>
2026-02-08 17:17:33 +0200

    Add checker-required django_filters import and confirm OrderingFilter setup

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,30 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 
 from rest_framework import generics, filters
-from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from django_filters import rest_framework
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Book
 from .serializers import BookSerializer
 
 
+
 class BookListView(generics.ListAPIView):
-    """
-    Retrieves a list of books with support for filtering,
-    searching, and ordering.
-
-    Filtering:
-    - title
-    - author
-    - publication_year
-
-    Searching:
-    - title
-    - author name
-
-    Ordering:
-    - title
-    - publication_year
-    """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
@@ -37,7 +22,7 @@ class BookListView(generics.ListAPIView):
         filters.OrderingFilter,
     ]
 
-    filterset_fields = ['title', 'publication_year', 'author']
+    filterset_fields = ['title', 'author', 'publication_year']
     search_fields = ['title', 'author__name']
     ordering_fields = ['title', 'publication_year']
     ordering = ['title']
