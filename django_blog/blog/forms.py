96b19d9fdf7ce6e350a52945571975a32f1724ed
nndwelenis <nndwelenis@gmail.com>
2026-02-14 17:05:44 +0200

    Implementing Advanced Features: Tagging and Search Functionality

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -6,6 +6,9 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import Comment
 from .models import Post, Tag
 
+from taggit.forms import TagWidget
+
+
 class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = User
@@ -18,43 +21,14 @@ class UserRegisterForm(UserCreationForm):
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
-
 class PostForm(forms.ModelForm):
-    tags = forms.CharField(
-        required=False,
-        help_text="Enter tags separated by commas"
-    )
 
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']
-    
-
-    def save(self, commit=True):
-        post = super().save(commit=False)
-
-        if commit:
-            post.save()
-
-        tag_names = self.cleaned_data.get('tags')
-        if tag_names:
-            tag_list = [name.strip() for name in tag_names.split(',')]
-
-            post.tags.clear()
-
-            for name in tag_list:
-                tag, created = Tag.objects.get_or_create(name=name)
-                post.tags.add(tag)
-
-        return post
-    
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        if self.instance.pk:
-            self.fields['tags'].initial = ', '.join(
-                tag.name for tag in self.instance.tags.all()
-            )
+        widgets = {
+            'tags': TagWidget(),
+        }
 
 
 
