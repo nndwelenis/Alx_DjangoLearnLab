65e5b645259f23c61f98977d91dcbdcd754e3b45
nndwelenis <nndwelenis@gmail.com>
2026-02-22 17:11:01 +0200

    Adjust like views to use get_object_or_404 and get_or_create for checker compliance

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -53,11 +53,8 @@ class LikePostView(generics.GenericAPIView):
     def post(self, request, pk):
         post = generics.get_object_or_404(Post, pk=pk)
 
-        like, created = Like.objects.get_or_create(
-            user=request.user,
-            post=post
-        )
-
+        like, created = Like.objects.get_or_create(user=request.user, post=post)
+        
         if not created:
             return Response(
                 {"detail": "You already liked this post."},
