8c95080cbe5ec50bd0d941486fd43778f1e030ea
nndwelenis <nndwelenis@gmail.com>
2026-02-22 21:46:37 +0200

    Explicit sqlite production database config with PORT

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,9 +83,11 @@ WSGI_APPLICATION = 'social_media_api.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "HOST": "localhost",
+        "PORT": "0",
     }
 }
 
