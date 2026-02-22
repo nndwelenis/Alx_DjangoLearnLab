372d12b7936e16904f5e136bcbeb291055ceace7
nndwelenis <nndwelenis@gmail.com>
2026-02-22 12:21:49 +0200

    Adjusted serializers and URL patterns to match checker requirements

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -19,11 +19,12 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'email', 'password']
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password']
         )
+
         Token.objects.create(user=user)
         return user
     
